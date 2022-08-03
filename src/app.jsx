2ea66203a3c13eeb7ba8a12fad69de30f8a49ce8
Samandar <samandarabdullaev393@gmail.com>
2022-08-03 13:33:44 +0500

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import { faker } from "@faker-js/faker";
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello CodeSandbox</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/issue.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Component from "react-component";
+class Issue extends Component {
+    state = {}
+    render() {
+        return (
+            <>
+                <h1>Hello Samandar</h1>
+            </>
+        );
+    }
+}
+
+export default Issue;
+
